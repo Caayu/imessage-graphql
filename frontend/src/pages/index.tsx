5e1b4b1f9a2872bab4742c345d5a66f859b07515
Caayu <henrycay@outlook.com>
2022-09-27 18:29:09 -0300

    Next Auth config and create login and sign out

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <div>Hello Home Page</div>;
+  const { data } = useSession();
+
+  console.log("HERE IS DATA", data);
+
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in</button>
+      )}
+      {data?.user?.name}
+    </div>
+  );
 };
 
 export default Home;
